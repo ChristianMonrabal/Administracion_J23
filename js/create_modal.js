e318360e3bf4ab562f28692abccdd0125865fc8d
Christian Monrabal <90915794+ChristianMonrabal@users.noreply.github.com>
2024-11-29 15:52:15 +0100

    add create modal js

---
+++ js/create_modal.js
@@ -0,0 +1,5 @@
+        $(document).ready(function() {
+            if ($('#crearAlumnoModal').data('open') === 'true') {
+                $('#crearAlumnoModal').modal('show');
+            }
+        });
